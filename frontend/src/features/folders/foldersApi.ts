3deeb9ce3bfb33f8498ad5b3d04c67139bc9de9d
OmarAhmedOwais <oa1476@fayoum.edu.eg>
2025-01-07 11:11:56 +0200

    feat: add folder apis in folder features

---
+++ frontend/src/features/folders/foldersApi.ts
@@ -0,0 +1,56 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { RootState } from '../../app/store';
+
+export interface Folder {
+  id: number;
+  name: string;
+  //notes?: Note[]
+}
+
+export const foldersApi = createApi({
+  reducerPath: 'foldersApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getFolders: builder.query<Folder[], void>({
+      query: () => '/folders',
+    }),
+    createFolder: builder.mutation<Folder, Partial<Folder>>({
+      query: (body) => ({
+        url: '/folders',
+        method: 'POST',
+        body,
+      }),
+    }),
+    updateFolder: builder.mutation<Folder, { id: number; data: Partial<Folder> }>(
+      {
+        query: ({ id, data }) => ({
+          url: `/folders/${id}`,
+          method: 'PATCH',
+          body: data,
+        }),
+      },
+    ),
+    deleteFolder: builder.mutation<void, number>({
+      query: (id) => ({
+        url: `/folders/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetFoldersQuery,
+  useCreateFolderMutation,
+  useUpdateFolderMutation,
+  useDeleteFolderMutation,
+} = foldersApi;
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,17 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../features/auth/LoginPage";
 import RegisterPage from "../features/auth/RegisterPage";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 function AppRoutes() {
   return (
     <Routes>
-      {/* Public routes */}
       <Route path='/login' element={<LoginPage />} />
-      {/* Public routes */}
       <Route path='/register' element={<RegisterPage />} />
-
-      {/* Default route */}
-      <Route path="*" element={<LoginPage />} />
+      <Route
+        path='/folders'
+        element={
+          <ProtectedRoute>
+            <FoldersPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<LoginPage />} />
     </Routes>
   );
 }
